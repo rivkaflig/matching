4b25f2148dbbf16e709a235085d7567f906a1ec4
rivka-flig <rivkaflig@gmail.com>
2025-03-11 18:25:14 -0400

    Images added to JS

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", ()=> {
     
+    const matches = [
+        {id: 1, gadol: Images/R-Chaim.jpg},
+        {id: 2, gadol: Images/R-Edelshtein.jpg},
+        {id: 3, gadol: Images/R-Elyashiv.jpg},
+        {id: 4, gadol: Images/R-Feinstein.jpg},
+        {id: 5, gadol: Images/R-Feldmen.jpg},
+        {id: 6, gadol: Images/R-Finkel.jpg},
+        {id: 7, gadol: Images/R-Salomon.jpg},
+        {id: 8, gadol: Images/R-Shapiro.jpg},
+        {id: 9, gadol: Images/R-Shteinman.jpg},
+        {id: 10, gadol: Images/Steipler-Gaon.jpg}
+    ];
+
+
 })
